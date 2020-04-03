43721022e029b0057a0fed579625d5c00f11bfc6
Pierre Segalen <pierre.segalen@gmail.com>
2020-04-03 14:23:47 +0200

    Correction d'un bug (désactivation du bouton manquante)

--- exercice1/etape3/index.js
+++ exercice1/etape3/index.js
@@ -11,6 +11,9 @@ function validerNom() {
   if (valeurNom.length == 0) {
     document.getElementById("erreur-nom").innerText =
       "Le nom est obligatoire !";
+    document
+      .getElementById("bouton-valider")
+      .setAttribute("disabled", "disabled");
   } else {
     document.getElementById("erreur-nom").innerText = "";
     var valeurAge = document.getElementById("age").value;
@@ -28,9 +31,15 @@ function validerAge() {
   if (valeurAge.length == 0) {
     document.getElementById("erreur-age").innerText =
       "L'âge est obligatoire !";
+    document
+      .getElementById("bouton-valider")
+      .setAttribute("disabled", "disabled");
   } else if (isNaN(valeurAge)) {
     document.getElementById("erreur-age").innerText =
       "L'âge doit être un nombre !";
+    document
+      .getElementById("bouton-valider")
+      .setAttribute("disabled", "disabled");
   } else {
     document.getElementById("erreur-age").innerText = "";
     var valeurNom = document.getElementById("nom").value;
