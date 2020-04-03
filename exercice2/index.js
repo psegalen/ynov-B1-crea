57073ec924d61e9a3d9a4fa521907d1d91e5ec0c
Pierre Segalen <pierre.segalen@gmail.com>
2020-04-03 15:47:19 +0200

    Algorithme de variation de l'animation : blanc / rouge / vert / bleu en boucle

--- exercice2/index.js
+++ exercice2/index.js
@@ -1,10 +1,25 @@
 var couleur = 15;
 var enTrainDeDescendre = true;
+var dominante = 0;
 
 function changerCouleur(indiceCouleur) {
   var indiceCouleurHexa = indiceCouleur.toString(16);
-  document.body.style.backgroundColor =
-    "#" + indiceCouleurHexa + indiceCouleurHexa + indiceCouleurHexa;
+  var bgColor = "";
+  if (dominante % 4 == 0) {
+    // première dominante : blanc
+    bgColor =
+      "#" + indiceCouleurHexa + indiceCouleurHexa + indiceCouleurHexa;
+  } else if (dominante % 4 == 1) {
+    // seconde dominante : rouge
+    bgColor = "#" + indiceCouleurHexa + "00";
+  } else if (dominante % 4 == 2) {
+    // troisième dominante : vert
+    bgColor = "#0" + indiceCouleurHexa + "0";
+  } else {
+    // quatrième dominante : bleu
+    bgColor = "#00" + indiceCouleurHexa;
+  }
+  document.body.style.backgroundColor = bgColor;
 }
 
 function algorithme() {
@@ -12,6 +27,7 @@ function algorithme() {
     couleur--;
     if (couleur == 0) {
       enTrainDeDescendre = false;
+      dominante++;
     }
   } else {
     couleur++;
